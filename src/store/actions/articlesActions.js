9d7d9a81c55d4696d65e6d400f81fb4006e0b06b
Sigmund Granaas <sigmundsgranaas@gmail.com>
2020-10-09 18:07:33 +0200

    * Adding todo's and documenting file

--- src/store/actions/articlesActions.js
+++ src/store/actions/articlesActions.js
@@ -1,6 +1,22 @@
+/**
+ * ArticlesActions
+ * 
+ * This file contains all actions regarding articles/posts
+ * 
+ */
+
 import {GET_ARTICLES, ARTICLES_ERROR, GET_SINGLE_ARTICLE} from '../types'
 import axios from 'axios'
 
+/**
+ * Action for fetching all availible posts from wordpress
+ * 
+ * Currently does not support pagination
+ * 
+ * TODO: Add support for pagination and/or infinity scroll
+ * 
+ * @return will save array of posts to state
+ */
 export const getArticles = () => async dispatch => {
 
     try{
@@ -14,11 +30,19 @@ export const getArticles = () => async dispatch => {
     catch(e){
         dispatch({
             type: ARTICLES_ERROR,
-            payload: console.log(e),
+            payload: console.log(e), // TODO improve logging to a usable level
         })
     }
 }
 
+
+/**
+ * Action for fetching a single article from wordpress with a specified ID.
+ * 
+ * Will return res.data wrapped in [] to make it an array.
+ * 
+ * @param {*} id id identofiyng a single post.
+ */
 export const getSingleArticle = (id) => async dispatch => {
     try{
         const res = await axios.get(`http://localhost:8000/wp-json/wp/v2/posts/${id}`);
@@ -30,7 +54,7 @@ export const getSingleArticle = (id) => async dispatch => {
     catch(e){
         dispatch({
             type: ARTICLES_ERROR,
-            payload: console.log(e),
+            payload: console.log(e), // TODO improve logging to a usable level
         })
     }
 }
