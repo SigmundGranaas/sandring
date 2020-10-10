985b364c306695bd687a5bb8f19c34314d278c22
SigmundGranaas <sigmundsgranaas@gmail.com>
2020-10-10 14:30:37 +0200

    * adding testing libraries * writing basic tests for app.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow, mount } from "enzyme";
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
-test('renders learn react link', () => {
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+test('render logo', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/logo/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+it("renders header elements", () => {
+  const wrapper = shallow(<App />);
+  const header = <Header/>;
+  expect(wrapper.contains(header)).toEqual(true);
+});
+
+it("renders footer elements", () => {
+  const wrapper = shallow(<App />);
+  const footer = <Footer/>;
+  expect(wrapper.contains(footer)).toEqual(true);
 });
