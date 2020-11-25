c78a4486fdbc200c060557ec5651e586fa2b0890
SigmundGranaas <sigmund@DESKTOP-TCO2I9C.localdomain>
2020-11-25 11:57:09 +0100

    * Created Header container * Created Header element builder * Wrote tests for all components

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import Gallery from  './containers/GalleryContainer';
 import GalleryImageContainer from './containers/GalleryImageContainer';
 import Blog from './components/Blog/controller/BlogContainer';
 import BlogEntry from './components/Blog/controller/BlogEntryContainer';
-import Header from './components/Header/Header';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Footer from './components/Footer/Footer';
+import headerMock from './components/Header/Mocks/headerMock.json'
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,17 +18,15 @@ import {
 function App() {
   return (
     <Router>
-      
-        <Switch>
-          <Header/>
-          <Route path="/" exact component={Landing}/>
-          <Route path="/gallery" exact component={Gallery}/>
-          <Route path="/gallery/:id"  component={GalleryImageContainer}/>
-          <Route path="/blog" exact component={Blog}/>
-          <Route path="/blog/:id" component={BlogEntry}/>
-          <Footer/>
-        </Switch>
-        
+      <HeaderContainer headerElements={headerMock}/>
+      <Switch>
+        <Route path="/" exact component={Landing}/>
+        <Route path="/gallery" exact component={Gallery}/>
+        <Route path="/gallery/:id"  component={GalleryImageContainer}/>
+        <Route path="/blog" exact component={Blog}/>
+        <Route path="/blog/:id" component={BlogEntry}/>
+      </Switch>
+      <Footer/>
     </Router>
   );
 }
